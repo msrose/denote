8db09e10dcb642058199b6bf72ec03d520f08785
Michael Rose <michael_rose@outlook.com>
2016-02-07 23:15:54 -0500

    Add Promises/A+ compliance tests package

---
+++ test/adapter.js
@@ -0,0 +1,14 @@
+var denote = require('../denote');
+
+exports.deferred = function() {
+  var promise = denote();
+  return {
+    promise: promise,
+    resolve: function(value) {
+      promise.resolve(value);
+    },
+    reject: function(reason) {
+      promise.reject(reason);
+    }
+  };
+};
